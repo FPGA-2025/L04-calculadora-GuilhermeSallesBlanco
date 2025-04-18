ced926d1f463895b25dc1b5e7b6a0e65f40b9a19
GuilhermeSallesBlanco <guilherme.salles.blanco@gmail>
2025-04-18 01:36:04 -0300

    Adicionando tarefas realizadas

--- calculadora.v
+++ calculadora.v
@@ -5,6 +5,11 @@ module calculadora(
   output  [7:0] saida
 );
 
-// insira seu código aqui
+assign saida = (codigo == 3'b000) ? 8'b00000000 : // Zera se código for 000
+               (codigo == 3'b001) ? entrada_A : // Mostra A se código for 001
+               (codigo == 3'b010) ? entrada_B : // Mostra B se código for 010
+               (codigo == 3'b011) ? entrada_A + entrada_B : // Soma A e B se código for 011
+               (codigo == 3'b100) ? entrada_A - entrada_B : // Subtrai A e B se código for 100
+               8'b00000000; // Zera se código for qualquer outro valor
 
 endmodule
